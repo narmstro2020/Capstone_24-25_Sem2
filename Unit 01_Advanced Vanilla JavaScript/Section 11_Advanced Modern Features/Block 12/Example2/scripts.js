882534f61f2d8fe1c569603cdaaa5c21b36913a6
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-11 06:57:43 -0500

    Block 13

--- Unit 01_Advanced Vanilla JavaScript/Section 11_Advanced Modern Features/Block 12/Example2/scripts.js
+++ Unit 01_Advanced Vanilla JavaScript/Section 11_Advanced Modern Features/Block 12/Example2/scripts.js
@@ -6,14 +6,17 @@ console.log(typeof true);
 console.log(typeof 20398472309487230984273094n);
 console.log(typeof Symbol("id"));
 console.log(typeof {name: "Armstrong", age: "ancient"});
-console.log(typeof [1, 2, 3, 4]);
+console.log([1, 2, 3, 4].constructor);
 console.log(typeof function() {
     return 3;
 });
-console.log(typeof new Date());
+console.log(new Date().constructor.name);
 console.log(typeof new Set([1, 2, 3, 4]));
 console.log(typeof new Error("Hi"));
 console.log(typeof new Map());
+
+console.log(Array.isArray([1, 2, 3, 4]));
+
 
 
 

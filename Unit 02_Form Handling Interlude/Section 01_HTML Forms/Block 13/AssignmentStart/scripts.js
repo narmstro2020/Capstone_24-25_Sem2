b25082851ea5f7a351347e1fff6f4d1c77da49ba
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-11 11:55:17 -0500

    Block 13

--- Unit 02_Form Handling Interlude/Section 01_HTML Forms/Block 13/AssignmentStart/scripts.js
+++ Unit 02_Form Handling Interlude/Section 01_HTML Forms/Block 13/AssignmentStart/scripts.js
@@ -1,5 +1,9 @@
 function validateForm() {
     let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    username.innerHTML = "";
+    password.innerHTML = "";
+
     if (username === "" && password === "") {
         document.getElementById("username-error").innerText = "Username is required";
         // TODO: mark both errors in the HTML

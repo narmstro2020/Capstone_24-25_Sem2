28f4984c44517ef7364cb0fa04468b6d9370488c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-21 11:54:32 -0500

    Block 16

---
+++ Unit 04_React_Basics/Section 01_Hello World/Hello_React/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hello React</h1>
+            <p>This was created with React from Scratch!</p>
+        </div>
+    );
+};
+
+export default App;
